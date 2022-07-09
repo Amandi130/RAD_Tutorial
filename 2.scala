24a2a91218446d0e67d5d6343a725166c370256f
Amandi130 <93074050+Amandi130@users.noreply.github.com>
2022-07-09 22:37:42 +0530

    Add files via upload

---
+++ 2.scala
@@ -0,0 +1,21 @@
+object  scala_2{
+  def main(args: Array[String]): Unit = {
+    val a=2;
+    val b=3;
+    val c=4;
+    val d=5;
+    val k=4.3;
+
+    println( "a)--b* a+c*d--"+ (b*a + c*d));
+    val z=b+1;
+    val x=d-1;
+    val w=a+1;
+    println("b)a++="+ (w));
+    println("c)-2*(d-k)+c="+ ((-2)*(x-k)+c));
+    println("d)c=c++="+ (c));
+    val e=c+1;
+    val g=c+1;
+    println("e)c=++c*a++="+ (g*w));
+    val h=a+1;
+  }
+}
