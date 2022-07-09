817b9ca194eca2dc543ee074c2d3fe9291e38766
Amandi130 <93074050+Amandi130@users.noreply.github.com>
2022-07-09 22:36:51 +0530

    Add files via upload

---
+++ 1.scala
@@ -0,0 +1,20 @@
+object scala_1 {
+  def main(args: Array[String]): Unit = {
+    val k=2;
+    val i=2;
+    val j=2;
+    val m=5;
+    val n=5;
+    val f=12.0f;
+    val g=4.0f;
+    val c="X";
+
+      println("a)"+ (k+12*m))
+      println("b)"+ (m/j))
+      println("c)"+ (n%j))
+      println("d)"+ (m/j*j))
+      println("e)"+ (f+10*5+g))
+      println("f)"+ ((i+1)*n))
+
+   }
+}
